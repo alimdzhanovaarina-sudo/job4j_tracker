ec3d6127ec783f2a7d3c398327490c6b73560cb8
Admin <alimdzhanova.arina@gmail.com>
2026-02-16 18:46:04 +0300

    иере

--- src/main/java/ru/job4j/tracker/CreateAction.java
+++ src/main/java/ru/job4j/tracker/CreateAction.java
@@ -8,15 +8,14 @@ public class CreateAction implements UserAction {
     public String name() {
         return "Добавить новую заявку";
     }
-    }
 
-@Override
-public boolean execute(Input input, Tracker tracker) {
-    System.out.println("=== Создание новой заявки ===");
-    String name = input.askStr("Введите имя: ");
-    Item item = new Item(name);
-    tracker.add(item);
-    System.out.println("Добавленная заявка: " + item);
-    return true;
-}
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        System.out.println("=== Создание новой заявки ===");
+        String name = input.askStr("Введите имя: ");
+        Item item = new Item(name);
+        tracker.add(item);
+        System.out.println("Добавленная заявка: " + item);
+        return true;
+    }
 }
