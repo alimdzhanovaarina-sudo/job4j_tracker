e865fe2d630eaa921b25a68a20ccb572d2e8f0c4
Admin <alimdzhanova.arina@gmail.com>
2026-02-18 21:41:15 +0300

    throw new фактоарил тест

---
+++ src/test/java/ru/job4j/ex/FactorialTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import ru.job4j.ex.Factorial;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class FactorialTest {
+    @Test
+    void whenException() {
+        Factorial factorial = new Factorial();
+
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> factorial.calc(-1)
+        );
+        assertThat(exception.getMessage()).isEqualTo("Number could not be less than 0");
+    }
+}
+
