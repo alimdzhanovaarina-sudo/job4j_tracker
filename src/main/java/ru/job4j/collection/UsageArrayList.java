f082d4cd0fa00b66c2661a2a281acd7e52d2c38b
Admin <alimdzhanova.arina@gmail.com>
2026-02-24 21:16:39 +0300

    array list

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -3,7 +3,7 @@ package ru.job4j.collection;
 import java.util.ArrayList;
 
 public class UsageArrayList {
-    public static void main {
+    public static void main (String[] args) {
         ArrayList<String> names = new ArrayList<String>();
 
         names.add("Petr");
