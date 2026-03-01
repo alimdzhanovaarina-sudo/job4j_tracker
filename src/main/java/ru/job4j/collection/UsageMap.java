c63545746f43ebe7bf52bf5a51d4a2ea206ed4fd
Admin <alimdzhanova.arina@gmail.com>
2026-03-01 20:21:10 +0300

    hashmap

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -5,7 +5,9 @@ import java.util.HashMap;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
-        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("petr@yandex.ru", "Petr Arsentev");
+        map.put("abc@yandex.ru", "Petr Arsentev");
+        map.put("petr@yandex.ru", "Peter Arsentev");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
