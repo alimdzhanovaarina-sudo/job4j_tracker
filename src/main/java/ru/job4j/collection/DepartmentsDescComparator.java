e01b1660d8ddbdc9ce704ba43f6741d41d575a12
Admin <alimdzhanova.arina@gmail.com>
2026-03-05 18:25:35 +0300

    компаратор по убыванию

--- src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -5,6 +5,17 @@ import java.util.Comparator;
 public class DepartmentsDescComparator implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        return right.compareTo(left);
+        String[] leftParts = left.split("/");
+        String[] rightParts = right.split("/");
+        String leftFirstPart = leftParts[0];
+        String rightFirstPart = rightParts[0];
+
+        int firstPartComparisonDesc = rightFirstPart.compareTo(leftFirstPart);
+        if (firstPartComparisonDesc == 0) {
+            return left.compareTo(right);
+        } else {
+            return firstPartComparisonDesc;
+        }
     }
 }
+
